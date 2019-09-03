3177c8e9a4e4049ab4faacb19ba3b40f63d5df61
dingdingboy <goldsmooth@126.com>
2019-09-03 12:20:46 +0800

    Add a new file

---
+++ power.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def power(self, a, n): # computer the n power of a
+        if n == 0:
+            return 1
+        tmp = self.power(a, int(n/2))
+        if n&1:
+            return tmp*tmp*a
+        else:
+            return tmp&tmp
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.power(2, 4))
