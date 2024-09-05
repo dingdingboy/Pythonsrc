7a63f0a456263aa9a802b81e84b23f0dd3d3b4e7
Jinping4Intc <goldsmooth@126.com>
2024-09-06 02:19:53 +0800

    add dummy code

--- power.py
+++ power.py
@@ -7,9 +7,16 @@ class Solution(object):
             return tmp*tmp*a
         else:
             return tmp&tmp
+        
+import torch
 
 if __name__ == "__main__":
     #add a comment
     print('hello world')
     obj = Solution()
     print(obj.power(2, 4))
+    x = torch.tensor(2.0)
+    y = torch.tensor(4.0)
+    print(torch.pow(x, y))
+    z= torch.rand(2,3)
+    print(z)
